0b68f950f0159b927ce424179ab87f8eca1fae0f
jesstphan <92838597+jesstphan@users.noreply.github.com>
2021-11-03 00:08:05 -0400

    Update lab5Background.py

--- lab5Background.py
+++ lab5Background.py
@@ -2,4 +2,12 @@ import RPi.GPIO as GPIO
 import time 
 import json 
 
-from stepper
+from stepper5 import Stepper
+
+Object = Stepper([18,21,22,23],26) #input pins and LED  
+
+while True:
+  try:
+    with open("
+
+
