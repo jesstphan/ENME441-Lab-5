41a37bd9eaae752274c23213f74922d30ccf1b27
jesstphan <92838597+jesstphan@users.noreply.github.com>
2021-11-03 14:36:29 -0400

    Update lab5Background.py

--- lab5Background.py
+++ lab5Background.py
@@ -8,6 +8,15 @@ Object = Stepper([18,21,22,23],26) #input pins and LED
 
 while True:
   try:
-    with open("
+    with open("lab5.txt", 'r') as f 
+    form = json.load(f) 
+    time.sleep(0.5)
+    print(form) 
+    
+ if form['slider1'] != None:
+  angle = int(form['slider1'])
+  Object.goAngle(angle)
+  
+ if str(form['zerobutton']) == 'Change 
 
 
